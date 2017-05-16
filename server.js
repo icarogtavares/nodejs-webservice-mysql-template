e38e076c8446284ac882225fed0f44e067e7578f
Icaro Tavares <icarotavares@live.com>
2017-05-16 13:25:59 -0300

    Using throng to cluster

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import setupApp from './src/app';
 function startApp(id) {
 	const port = process.env.PORT || 4000;
 	setupApp()
-		.listen(port, () => console.log(`App running at http://localhost:${port}`))
+		.listen(port, () => console.log(`App running at port ${port}`))
 }
 
 export default startApp;
